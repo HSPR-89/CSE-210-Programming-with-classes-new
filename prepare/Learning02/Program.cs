cf7c11e6370c67d2f4a53b69dcc640279b57f741
perei <pereirahoward@hotmail.com>
2024-05-25 19:55:58 -0600

    New program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,26 +1,59 @@
 using System;
+using System.Runtime.Serialization.Formatters;
+
 class Program
 {
-    static void Main(string[] args)
+    class Job
     {
-        Job job1 = new Job();
-        job1._jobTitle = "Equipment Engineer";
-        job1._company = "Boston Scientific";
-        job1._startYear = 2014;
-        job1._endYear = 2019;
-
-        Job job2 = new Job();
-        job2._jobTitle = "Transfer Engineer";
-        job2._company = "Edwards Lifesciences";
-        job2._startYear = 2020;
-        job2._endYear = 2022;
-
-        Resume myResume = new Resume();
-        myResume._name = "Howard Pereira";
-
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
-
-        myResume.Display();
+        public string _company = "";
+        public string _jobTitle = "";
+        public int _startYear = 0;
+        public int _endYear = 0;
+        public Job()
+        {
+        }
+        public void display()
+        {
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}.");
+        }
     }
+
+    class Resume
+    {
+        public string _name = "";
+        public List<Job> _jobs = new List<Job>();
+        public Resume()
+        {
+
+        }
+        public void display()
+        {
+            Console.WriteLine($"Name:{_name} \nJobs: ");
+            foreach (Job line in _jobs)
+            {
+                line.display();
+            }
+        }
+    }
+    static void Main(string[] args)
+{
+    Job Howard = new Job();
+    Howard._company = "Boston Scientific";
+    Howard._jobTitle = "Equipment Engineer";
+    Howard._startYear = 2014;
+    Howard._endYear = 2019;
+
+    Job Howard1 = new Job();
+    Howard1._company = "Edwards Lifesciences";
+    Howard1._jobTitle = "Equipment Transfer Engineer";
+    Howard1._startYear = 2020;
+    Howard1._endYear = 2022;
+
+    Resume CV= new Resume();
+    CV._name= "Howard Pereira";
+    CV._jobs.Add(Howard);
+    CV._jobs.Add(Howard1);
+    CV.display();
 }
+}
+
